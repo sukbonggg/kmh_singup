a9b310a9fb4aa8e7e9c110456ea4a04348ad08be
sukbonggg <kiti982611@gmail.com>
2023-01-10 21:43:03 +0900

    회원가입 폼 완성

--- src/main/java/com/kmh/exam/demo/KmhTestss1Application.java
+++ src/main/java/com/kmh/exam/demo/KmhTestss1Application.java
@@ -2,12 +2,18 @@ package com.kmh.exam.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KmhTestss1Application {
+public class KmhTestss1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KmhTestss1Application.class, args);
 	}
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(KmhTestss1Application.class);
+	  }
 
 }
--- src/main/java/com/kmh/exam/demo/config/MvcConfiguration.java
+++ src/main/java/com/kmh/exam/demo/config/MvcConfiguration.java
@@ -4,10 +4,12 @@ package com.kmh.exam.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
 public class MvcConfiguration implements WebMvcConfigurer {
 
 	@Override 
@@ -21,4 +23,14 @@ public class MvcConfiguration implements WebMvcConfigurer {
 		/* '/font/**'로 호출하는 자원은 '/static/font/' 폴더 아래에서 찾는다. */ 
         registry.addResourceHandler("/vendor/**").addResourceLocations("classpath:/static/vendor/").setCachePeriod(60 * 60 * 24 * 365); 
 	}
+
+/*    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+
+    }*/
 }
